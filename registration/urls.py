6960a2e89c32f540e0cf928d337e0ba2539624f6
bindubhargavi568 <bindubhargavi568@gmail.com>
2021-10-25 12:47:34 +0530

    Created APIs using django rest frame work

--- registration/urls.py
+++ registration/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from registration import views
+from .views import Register, Login
 
 urlpatterns = [
-    path('registration', views.user_register, name='register'),
-    path('login', views.user_login, name='login')
-
+    path('registration', Register.as_view(), name='register'),
+    path('login', Login.as_view(), name='login')
 ]
